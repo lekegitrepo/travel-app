e699288f87b62afdbbac9cb8955d8be217e38a6f
lekegitrepo <lekedevrepo@gmail.com>
2020-04-04 10:43:40 -0500

    set the view engine

--- tour-app.js
+++ tour-app.js
@@ -2,6 +2,11 @@ let express = require('express');
 
 let app = express();
 
+let handlebars = require('express3-handlebars').create({ defaultLayout: 'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars')
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
