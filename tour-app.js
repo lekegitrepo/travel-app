bc9d5f2e436191961ee5f6b699621d4e73632993
lekegitrepo <lekedevrepo@gmail.com>
2020-04-17 12:36:12 -0500

    Moved 'fortune cookie' functionality into module. import fortune in the tour-app.js file

--- tour-app.js
+++ tour-app.js
@@ -1,4 +1,5 @@
 let express = require('express');
+let fortune = require('./lib/fortune.js');
 
 let app = express();
 
@@ -11,21 +12,21 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
-let fortunes = [
+/*let fortunes = [
  "Conquer your fears or they will conquer you.",
  "Rivers need springs.",
  "Do not fear what you don't know.",
  "You will have a pleasant surprise.",
  "Whenever possible, keep it simple.",
-];
+];*/
 
 app.get('/', function(req, res){
   res.render('home');
 })
 
 app.get('/about', function(req, res){
-  let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', {fortune: randomFortune});
+  //let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', {fortune: fortune.getFortune()});
 })
 
 // custom 404 page
