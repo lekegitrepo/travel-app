83f8c3a534de0674ab9d2c9e7753b778644440df
lekegitrepo <lekedevrepo@gmail.com>
2020-07-03 14:49:19 -0500

    create vacation-photo.handlebars

--- tour-app.js
+++ tour-app.js
@@ -61,6 +61,20 @@ app.get('/headers', (req, res) => {
   res.send(s);
 })
 
+app.use(require('body-parser')());
+
+app.get('/newsletter', function(req, res){
+  res.render('newsletter', {csrf: 'CSRF token goes here'})
+});
+
+app.post('/process', function(req, res) {
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you');
+});
+
 // custom 404 page
 app.use(function(req, res){
   res.status(404);
